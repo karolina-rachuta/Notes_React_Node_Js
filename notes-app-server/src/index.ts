a9387f2c44ae3892016f0ac2fd67f1c6c97b5c8c
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-08-11 22:15:12 +0400

    endpoint to update the note

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -18,12 +18,35 @@ app.post("/api/notes", async (req, res) => {
   if (!title || !content) {
     return res.status(400).send("title and content fields required");
   }
-  
+
   try {
     const note = await prisma.note.create({
       data: { title, content },
     });
     res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops something went wrong");
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if(!title || !content) {
+    return res
+    .status(400).send("title and content fields required");
+  }
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("Id must be a valid number");
+  }
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.json(updatedNote);
   } catch (error) {
     res.status(500).send("Oops something went wrong");
   }
